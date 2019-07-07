5409aaf8b555401c1efa157ea445a8cef21f88c4
Tuan <woodrufftuan@gmail.com>
2019-07-06 22:39:15 -0700

    Update script.js

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const winningCombos = [
     [2,4,6], [0,3,6], [1,4,7], [2,5,8]
 ];
 
+var currentPlayer, gamePlaying;
 
 
 
@@ -20,69 +21,76 @@ function checkForWinner(){
     for(var i = 0; i < 9; i++){
         var filled = document.getElementById('box-' + i).textContent;
         if(filled === ''){
-            nextPlayer();
-        }
+            gamePlaying = true;
+        }else{
+            gamePlaying = false;
+            console.log('game over');        }
     }
 }
 
 
-function nextPlayer(currentPlayer){
+function nextPlayer(){
     if(currentPlayer === 'X'){
         currentPlayer = 'O';
     }else{
         currentPlayer = 'X';
     }
-    playerClicked(currentPlayer);
-}
-
-
-function playerClicked(currentPlayer){
-    document.getElementById('box-0').addEventListener('click', function(){
-        document.getElementById('box-0').textContent = currentPlayer;
-    })
-    document.getElementById('box-1').addEventListener('click', function(){
-        document.getElementById('box-1').textContent = currentPlayer;
-    })
-    document.getElementById('box-2').addEventListener('click', function(){
-        document.getElementById('box-2').textContent = currentPlayer;
-    })
-    document.getElementById('box-3').addEventListener('click', function(){
-        document.getElementById('box-3').textContent = currentPlayer;
-    })
-    document.getElementById('box-4').addEventListener('click', function(){
-        document.getElementById('box-4').textContent = currentPlayer;
-    })
-    document.getElementById('box-5').addEventListener('click', function(){
-        document.getElementById('box-5').textContent = currentPlayer;
-    })
-    document.getElementById('box-6').addEventListener('click', function(){
-        document.getElementById('box-6').textContent = currentPlayer;
-    })
-    document.getElementById('box-7').addEventListener('click', function(){
-        document.getElementById('box-7').textContent = currentPlayer;
-    })
-    document.getElementById('box-8').addEventListener('click', function(){
-        document.getElementById('box-8').textContent = currentPlayer;
-    })
-    checkForWinner(currentPlayer);
+    checkForWinner();
 }
 
 
 
-document.querySelector('.button-1').addEventListener('click', function() {
-    init();
+document.getElementById('box-0').addEventListener('click', function(){
+    document.getElementById('box-0').textContent = currentPlayer;
+    nextPlayer();
+})
+document.getElementById('box-1').addEventListener('click', function(){
+    document.getElementById('box-1').textContent = currentPlayer;
+    nextPlayer();
+})
+document.getElementById('box-2').addEventListener('click', function(){
+    document.getElementById('box-2').textContent = currentPlayer;
+    nextPlayer();
+})
+document.getElementById('box-3').addEventListener('click', function(){
+    document.getElementById('box-3').textContent = currentPlayer;
+    nextPlayer();
+})
+document.getElementById('box-4').addEventListener('click', function(){
+    document.getElementById('box-4').textContent = currentPlayer;
+    nextPlayer();
+})
+document.getElementById('box-5').addEventListener('click', function(){
+    document.getElementById('box-5').textContent = currentPlayer;
+    nextPlayer();
+})
+document.getElementById('box-6').addEventListener('click', function(){
+    document.getElementById('box-6').textContent = currentPlayer;
+    nextPlayer();
+})
+document.getElementById('box-7').addEventListener('click', function(){
+    document.getElementById('box-7').textContent = currentPlayer;
+    nextPlayer();
+})
+document.getElementById('box-8').addEventListener('click', function(){
+    document.getElementById('box-8').textContent = currentPlayer;
+    nextPlayer();
 })
 
 
 
+document.querySelector('.button-1').addEventListener('click', init);
+
+
+
 function init(){
+    currentPlayer = 'X';
+    gamePlaying = true;
+    
+    
     for (var i = 0; i < 9; i++) {
         document.getElementById('box-' + i).textContent = '';
     }
-    
-    playerClicked('X');
-    
-    
 }
 
 
