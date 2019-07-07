54e1f19c556b8f50703453206ae836569a0cc69b
Tuan <woodrufftuan@gmail.com>
2019-07-06 23:31:46 -0700

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,52 @@ const winningCombos = [
     [2,4,6], [0,3,6], [1,4,7], [2,5,8]
 ];
 
-var currentPlayer, gamePlaying;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*var currentPlayer, gamePlaying, X, O;
 
 
 
@@ -18,19 +63,12 @@ var currentPlayer, gamePlaying;
 
 
 function checkForWinner(){
-    for(var i = 0; i < 9; i++){
-        var filled = document.getElementById('box-' + i).textContent;
-        if(filled === ''){
-            gamePlaying = true;
-        }else{
-            gamePlaying = false;
-            console.log('game over');        }
-    }
+   
 }
 
 
 function nextPlayer(){
-    if(currentPlayer === 'X'){
+    if(currentPlayer === X){
         currentPlayer = 'O';
     }else{
         currentPlayer = 'X';
@@ -39,43 +77,52 @@ function nextPlayer(){
 }
 
 
-
-document.getElementById('box-0').addEventListener('click', function(){
-    document.getElementById('box-0').textContent = currentPlayer;
-    nextPlayer();
-})
-document.getElementById('box-1').addEventListener('click', function(){
-    document.getElementById('box-1').textContent = currentPlayer;
-    nextPlayer();
-})
-document.getElementById('box-2').addEventListener('click', function(){
-    document.getElementById('box-2').textContent = currentPlayer;
-    nextPlayer();
-})
-document.getElementById('box-3').addEventListener('click', function(){
-    document.getElementById('box-3').textContent = currentPlayer;
-    nextPlayer();
-})
-document.getElementById('box-4').addEventListener('click', function(){
-    document.getElementById('box-4').textContent = currentPlayer;
-    nextPlayer();
-})
-document.getElementById('box-5').addEventListener('click', function(){
-    document.getElementById('box-5').textContent = currentPlayer;
-    nextPlayer();
-})
-document.getElementById('box-6').addEventListener('click', function(){
-    document.getElementById('box-6').textContent = currentPlayer;
-    nextPlayer();
-})
-document.getElementById('box-7').addEventListener('click', function(){
-    document.getElementById('box-7').textContent = currentPlayer;
-    nextPlayer();
-})
-document.getElementById('box-8').addEventListener('click', function(){
-    document.getElementById('box-8').textContent = currentPlayer;
-    nextPlayer();
-})
+function activeGame(){
+    if(gamePlaying){
+        document.getElementById('box-0').addEventListener('click', function(){
+            document.getElementById('box-0').textContent = currentPlayer;
+            nextPlayer();
+        })
+        document.getElementById('box-1').addEventListener('click', function(){
+            document.getElementById('box-1').textContent = currentPlayer;
+            nextPlayer();
+        })
+        document.getElementById('box-2').addEventListener('click', function(){
+            document.getElementById('box-2').textContent = currentPlayer;
+            nextPlayer();
+        })
+        document.getElementById('box-3').addEventListener('click', function(){
+            document.getElementById('box-3').textContent = currentPlayer;
+            nextPlayer();
+        })
+        document.getElementById('box-4').addEventListener('click', function(){
+            document.getElementById('box-4').textContent = currentPlayer;
+            nextPlayer();
+        })
+        document.getElementById('box-5').addEventListener('click', function(){
+            document.getElementById('box-5').textContent = currentPlayer;
+            nextPlayer();
+        })
+        document.getElementById('box-6').addEventListener('click', function(){
+            document.getElementById('box-6').textContent = currentPlayer;
+            nextPlayer();
+        })
+        document.getElementById('box-7').addEventListener('click', function(){
+            document.getElementById('box-7').textContent = currentPlayer;
+            nextPlayer();
+        })
+        document.getElementById('box-8').addEventListener('click', function(){
+            document.getElementById('box-8').textContent = currentPlayer;
+            nextPlayer();
+        })
+    }else{
+        console.log('GAME OVER');
+    }
+    
+    
+    
+    
+}
 
 
 
@@ -85,15 +132,18 @@ document.querySelector('.button-1').addEventListener('click', init);
 
 function init(){
     currentPlayer = 'X';
+    X = 'X';
+    O = 'O';
     gamePlaying = true;
     
     
     for (var i = 0; i < 9; i++) {
         document.getElementById('box-' + i).textContent = '';
     }
+    activeGame();
 }
 
-
+*/
 
 
 
